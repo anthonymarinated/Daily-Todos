31f2667bbc273c607aa983ae6be2e3698ad48950
anthonymarinated <anthonymarin718@gmail.com>
2022-10-26 19:15:08 -0400

    client side is now working with backend to send db queries

--- components/listTodo.js
+++ components/listTodo.js
@@ -13,6 +13,16 @@ export default function ListTodo(){
         }
     }
 
+    const deleteTodo = async (id) => {
+        try {
+            const deleteTodo = await fetch(`http://localhost:3000/todos/${id}`, {
+                method: 'DELETE'
+            });
+            setTodos(todos.filter(todo => todo.todo_id !== id))
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
     useEffect(() => {
         getTodos();
     },[])
@@ -20,12 +30,42 @@ export default function ListTodo(){
 
     return (
         <div>
-            <h1>Hi</h1>
-            <ul>
+        <table className="table">
+             <thead>
+                <tr>
+                    <th>Description</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+                {/* <tr>
+                    <td>John</td>
+                    <td>Doe</td>
+                    <td>john@example.com</td>
+                </tr> */}
+                {
+                    todos.map(todo => (
+                        <tr key={todo.todo_id}>
+                            <td>{todo.description}</td>
+                            <td>Edit</td>
+                            <td><button 
+                                    className="btn btn-danger"
+                                    onClick={() => deleteTodo(todo.todo_id)}
+                                >
+                                Delete
+                                </button>
+                            </td>
+                        </tr>
+                    ))
+                }
+            </tbody>
+        </table>
+            {/* <ul>
                     {
                         todos.map(todo => <li key={todo.todo_id}>{todo.todo_id}, {todo.description}</li>)
                     }
-            </ul>
+            </ul> */}
         </div>
     )
 }
